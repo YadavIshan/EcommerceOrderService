61ec9744443fba89a825b0da5f14f46bb0a28c45
Ishan Yadav <37396882+YadavIshan@users.noreply.github.com>
2026-01-31 00:00:03 +0530

    Enabled service discovery

--- src/main/java/com/ishan/ecommerce/OrderService/OrderServiceApplication.java
+++ src/main/java/com/ishan/ecommerce/OrderService/OrderServiceApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableDiscoveryClient
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
